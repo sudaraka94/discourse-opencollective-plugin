f93f6e6bd070068548c898e1f3ec420478d353f7
sudaraka94 <sudarakayasindu@gmail.com>
2017-03-28 01:28:40 +0530

    Admin menu entry created

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-opencollective-plugin
+# about: This is intended to be a feature-rich plugin for opencollective-discourse integration
+# version: 1.0.0
+# authors: Sudaraka Jayathilaka
+# url: https://github.com/sudaraka94/discourse-opencollective-plugin.git
